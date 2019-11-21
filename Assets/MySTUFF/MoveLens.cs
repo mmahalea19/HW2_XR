2455b1fe12952baae35819667aa88532b087f1d8
MAHA_LINUX <mmahalea19@student.oulu.fi>
2019-11-21 11:37:15 +0200

    Need to add grab, think the tracking works pretty good

---
+++ Assets/MySTUFF/MoveLens.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class MoveLens : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject mainCamera;
+    public GameObject mirrorCamera;
+    public GameObject mirror;
+    private OVRCameraRig ovrCam;
+    void Start()
+    {
+        this.mainCamera = GameObject.Find("OVRCameraRig");
+        this.mirrorCamera = GameObject.Find("Camera");
+        this.mirror = GameObject.Find("Glass");
+        ovrCam = GameObject.FindObjectOfType<OVRCameraRig>();
+    }
+
+    void checkMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("DOwn");
+            this.mirror.transform.Translate(Vector3.down*(float)0.1,Space.Self);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("DOwn");
+            this.mirror.transform.Translate(Vector3.up*(float)0.1,Space.Self);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Debug.Log("DOwn");
+            this.mirror.transform.Translate(Vector3.left*(float)0.1,Space.Self);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("DOwn");
+            this.mirror.transform.Translate(Vector3.right*(float)0.1,Space.Self);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//        ovrCam.
+        //Debug.Log(mirror);
+        checkMovement();
+        Vector3 up=Vector3.up*(float)0.3;
+        Vector3 z =( mirror.transform.localPosition+Vector3.forward + Vector3.up*mirror.transform.position.y);
+       
+     
+
+        Vector3 center = mirror.transform.TransformPoint(z );
+       // Debug.Log(center);
+        
+        mirrorCamera.transform.LookAt(center);
+
+    }
+}
---
+++ Assets/MySTUFF/StopTracking.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.MCParent=GameObject.Find("Mock");
+        this.mainCamera = Camera.FindObjectOfType<OVRCameraRig>();
+    }
+
+    public bool verticalTrack = false;
+    public bool horizontalTrack = false;
+    public GameObject MCParent;
+    public OVRCameraRig mainCamera;
+    void checkButtons()
+    {
+       
+    }
+
+    void resetCamera()
+    {
+        mainCamera. transform.localRotation =Quaternion.Euler(Vector3.zero);
+        mainCamera.transform.localPosition = Vector3.zero;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (verticalTrack)
+        {
+            Vector3 mcPosition = mainCamera.transform.localPosition;
+
+            Quaternion camRot = mainCamera.transform.localRotation;
+            Vector3 eulerAngles = camRot.eulerAngles;
+            eulerAngles.x = 0;//MCParent.transform.rotation.eulerAngles.x;
+            eulerAngles.z = 0; //MCParent.transform.rotation.eulerAngles.z;
+            MCParent.transform.rotation = MCParent.transform.rotation  *Quaternion.Euler(eulerAngles);
+            
+            
+            
+            mcPosition.y = 0;
+            MCParent.transform.position += mcPosition; 
+            
+            
+            
+            
+        }
+        else if (horizontalTrack)
+        {
+            Vector3 mcPosition = mainCamera.transform.localPosition;
+            Quaternion camRot = mainCamera.transform.localRotation;
+            Vector3 eulerAngles = camRot.eulerAngles;
+            eulerAngles.y = 0;//MCParent.transform.rotation.eulerAngles.x;
+           
+            MCParent.transform.rotation = MCParent.transform.rotation  *Quaternion.Euler(eulerAngles);
+            
+            
+            
+           
+            mcPosition.x = 0;
+            mcPosition.z = 0;
+            MCParent.transform.position += mcPosition; 
+        }
+        else
+        {
+            
+            MCParent.transform.rotation = MCParent.transform.rotation* mainCamera.transform.localRotation;
+            MCParent.transform.position += mainCamera.transform.localPosition;
+           
+        
+        }
+        resetCamera();
+        
+        
+        
+        
+        
+    }
+}
