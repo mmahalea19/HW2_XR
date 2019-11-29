8e9996f22c267f291c700409df9b76d9985c9d20
Mihai Mahalean <mmahalea19@student.oulu.fi>
2019-11-29 13:51:50 +0200

    Working lens and demon

---
+++ Assets/MySTUFF/SetChild.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetChild : MonoBehaviour
+{
+
+
+
+    private bool isColliderAdded = false;
+    private Transform rightHand, leftHand;
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rightHand = this.GetComponent<Transform>().Find("hand_right");
+        leftHand = this.GetComponent<Transform>().Find("hand_left");
+        if (leftHand != null && rightHand != null && !isColliderAdded)
+        {
+            this.GetComponent<Transform>().Find("AvatarGrabberRight").transform.position = new Vector3(0, 0, 0);
+            this.GetComponent<Transform>().Find("AvatarGrabberRight").transform.parent = rightHand.transform;
+            this.GetComponent<Transform>().Find("AvatarGrabberLeft").transform.position = new Vector3(0, 0, 0);
+            this.GetComponent<Transform>().Find("AvatarGrabberLeft").transform.parent = leftHand.transform;
+            isColliderAdded = true;
+        }
+    }
+
+
+}
