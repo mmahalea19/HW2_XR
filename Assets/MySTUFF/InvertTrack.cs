3f19ba9bb9b99f7be64b772514686a6b0d299580
MAHA_LINUX <mmahalea19@student.oulu.fi>
2019-11-30 23:56:26 +0200

    Finished with tracking disable

---
+++ Assets/MySTUFF/InvertTrack.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertTrack : MonoBehaviour
+{
+    public bool stopRotation = false;
+    public bool stopTranslation = false;
+    public OVRPlayerController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.controller = GameObject.FindObjectOfType<OVRPlayerController>();
+    }
+
+    void checkButtons()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            stopRotation=!stopRotation;
+            this.initalRotEuler = this.controller.transform.rotation.eulerAngles;
+            initialRot = this.controller.transform.rotation;
+            initialMockPos = this.transform.rotation;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            stopTranslation=!stopTranslation;
+            initialPos = this.controller.transform.position;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+           this.controller.transform.Rotate(Vector3.up,1);
+        }
+    }
+
+    public Quaternion initialMockPos;
+    public Vector3 initialPos;
+    public Quaternion initialRot;
+
+    public Vector3 initalRotEuler;
+    // Update is called once per frame
+    void Update()
+    {
+        checkButtons();
+//        if (stopRotation)
+//        {
+//            Quaternion diff = this.controller.transform.rotation *Quaternion.Inverse(initialRot);
+//            this.transform.rotation *= Quaternion.Inverse(diff);
+//            this.initialRot = this.controller.transform.rotation;
+//
+//        }
+       if (stopRotation)
+       {
+//           Quaternion diff = this.controller.transform.rotation * Quaternion.Inverse(initialRot);
+//           this.transform.rotation=initialMockPos*Quaternion.Inverse(diff);
+           this.controller.transform.rotation = initialRot;
+           // this.initalRotEuler = this.controller.transform.rotation.eulerAngles;
+
+       }
+
+        if (stopTranslation)
+        {
+            Vector3 diff = this.controller.transform.position - initialPos;
+            this.transform.position -= diff;
+        }
+
+
+    }
+}
